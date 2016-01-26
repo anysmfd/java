93017e88b2205567a977284bb9d83b0e910f612c
anysmfd <cross@ukr.net>
2016-01-26 14:11:49 +0200

    user input

---
+++ Calculator.java
@@ -0,0 +1,19 @@
+/**
+класс реализует калькулятор
+*/
+public class Calculator{
+	private int result;
+	
+	public void add(int ... params){
+		for(Integer param : params){
+			this.result+= param;
+		}
+	}
+	public int getResult(){
+		return this.result;
+	}
+	
+	public void cleanresult(){
+		this.result = 0;
+	}
+}
--- calculate.java
+++ calculate.java
@@ -1,5 +1,12 @@
+/*
+Класс для вычисления суммы двух чисел. Числа берутся из входящего массива
+*/
 public class calculate {
-	public static void main (String [] args){
+	public static void main (String [] arg){
 		System.out.println("Calculate.....");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int sum=first+second;
+		System.out.println("Sum = "+sum);
 	}
 }
